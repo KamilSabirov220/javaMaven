b5ec830474f1248e3207192ef2af77fb0b3c6f74
Камиль <sabirovkamil93@gmail.com>
2023-06-14 17:12:42 +0300

    Выполнил ДЗ21

--- src/main/java/lesson21/DZ21.java
+++ src/main/java/lesson21/DZ21.java
@@ -4,7 +4,9 @@ import lesson16.Person;
 import lesson16.PersonApi;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class DZ21 {
@@ -16,15 +18,36 @@ public class DZ21 {
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------");
 
         //Лист Женщин
-        List<Person> famale = personFromApi.stream()
-                .filter(person -> person.getGender().equals("famale"))
+        List<Person> female = personFromApi.stream()
+                .filter(person -> person.getGender().equals("female"))
                 .toList();
-        famale.forEach(System.out::println);
+        female.forEach(System.out::println);
 
-        //Количество
+        //Cамая взрослая женщина
+        System.out.print("Cамой взрослой женщине "
+                + female.stream().mapToInt(Person::getAge).max()
+                + "лет");
+
+
+        //Cамая молодая женщина
+        System.out.print("Cамой молодой женщине "
+                + female.stream().mapToInt(Person::getAge).min()
+                + "лет");
+
+
+        //Количествов списке
+        System.out.println("Количество женщин в списке");
+        System.out.println(female.stream().distinct().count());
 
 
         //Средний возраст
+        System.out.println("Средний возраст женщин");
+        female.stream().mapToInt(Person::getAge).average().ifPresent(System.out::println);
+
+        //Сумма возростов женщин
+        System.out.println("Сумма возростов женщин");
+        female.stream().map(Person::getAge)
+                .reduce((acc, x) -> (acc + x)).ifPresent(System.out::println);
 
 
         //Лист Мужчин
@@ -32,6 +55,31 @@ public class DZ21 {
                 .filter(person -> person.getGender().equals("male"))
                 .toList();
 
+        //Cамый взрослый мужчина
+        System.out.print("Cамому взрослому мужчине "
+                + male.stream().mapToInt(Person::getAge).max()
+                + "лет");
+
+
+        //Cамый молодой мужчина
+        System.out.print("Cамому молодому мужчине "
+                + male.stream().mapToInt(Person::getAge).min()
+                + "лет");
+
+
+        //Количествов списке
+        System.out.println("Количество мужчин в списке");
+        System.out.println(male.stream().distinct().count());
+
+
+        //Средний возраст
+        System.out.println("Средний возраст мужчин");
+        male.stream().mapToInt(Person::getAge).average().ifPresent(System.out::println);
+
+        //Сумма возростов женщин
+        System.out.println("Сумма возростов мужчин");
+        male.stream().map(Person::getAge)
+                .reduce((acc, x) -> (acc + x)).ifPresent(System.out::println);
 
 
     }
